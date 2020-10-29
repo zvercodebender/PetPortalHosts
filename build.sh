298947a5cd5a80f6b05ba717de994e2d2608c83c
Admin <rbroker@digital.ai>
2020-10-29 14:46:50 -0400

    set end point

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ zip -r ../aws.ec2_instance.zip ./*
 cd ../..
 curl -LO https://dist.xebialabs.com/public/xl-cli/$CLI_VERSION/linux-amd64/xl
 chmod +x xl
-./xl apply --xl-deploy-username=$XLD_USER --xl-release-password=$XLD_PASSWD --file petportalhosts.yaml --values version=$VERSION
+./xl apply --xl-deploy-url=https://xlr.rbroker.linkpc.net --xl-deploy-username=$XLD_USER --xl-release-password=$XLD_PASSWD --file petportalhosts.yaml --values version=$VERSION
 
 rm artifacts/aws.ec2_instance.zip
