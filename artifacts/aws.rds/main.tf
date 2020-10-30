7b2d30716083236e8aafc55fbfb636c792374247
Admin <rbroker@digital.ai>
2020-10-30 13:19:34 -0400

    add more output variables

--- artifacts/aws.rds/main.tf
+++ artifacts/aws.rds/main.tf
@@ -8,14 +8,13 @@ provider "aws" {
 
 resource "aws_db_instance" "mysqldb" {
   allocated_storage    = 100
-  db_subnet_group_name = "default"
   engine               = "mysql"
   engine_version       = "5.7.19"
   identifier           = "mysqldb"
   instance_class       = "db.t2.micro"
   password             = "{{DB_USERNAME}}"
   skip_final_snapshot  = true
-  storage_encrypted    = true
+  storage_encrypted    = false
   username             = "{{DB_PASSWORD}}"
   vpc_security_group_ids = ["sg-0ede761da972c2900", "sg-0755f4c4ecea46847"]
 }
--- artifacts/aws.rds/outputs.tf
+++ artifacts/aws.rds/outputs.tf
@@ -1,7 +1,15 @@
 # Output variable definitions
 #######################################################
 #  MySQL Variables
-output "webserver_public_ip" {
+output "db_endpoint" {
+  description = "Public IP addresses of MySQL instances"
+  value       = "${aws_db_instance.mysqldb.endpoint}"
+}
+output "db_address" {
   description = "Public IP addresses of MySQL instances"
   value       = "${aws_db_instance.mysqldb.address}"
+}
+output "db_port" {
+  description = "Public IP addresses of MySQL instances"
+  value       = "${aws_db_instance.mysqldb.port}"
 }
