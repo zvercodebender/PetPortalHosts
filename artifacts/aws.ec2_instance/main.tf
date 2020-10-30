41d929e0aba585e07d37bcf109bf50afb1e44f31
Admin <rbroker@digital.ai>
2020-10-30 09:16:35 -0400

    change private key

--- artifacts/aws.ec2_instance/main.tf
+++ artifacts/aws.ec2_instance/main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "appserver" {
 
   associate_public_ip_address = true
     tags = {
-      Name = "rrb-webserver"
+      Name = "rrb-appserver"
       Terraform   = "true"
       Environment = "dev"
     }
@@ -66,7 +66,7 @@ resource "aws_instance" "appserver" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = "${file("/home/rick/Documents/keypass/rbroker-us1.pem")}"
+      private_key = "/home/rick/Documents/keypass/rbroker-us1.pem
       host        = "${self.public_dns}"
     }
     provisioner "file" {
@@ -76,7 +76,7 @@ resource "aws_instance" "appserver" {
       connection {
         type        = "ssh"
         user        = "ec2-user"
-        private_key = "${file("/home/rick/Documents/keypass/rbroker-us1.pem")}"
+        private_key = "/home/rick/Documents/keypass/rbroker-us1.pem
         host        = "${self.public_dns}"
       }
 
