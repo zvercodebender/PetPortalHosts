3e3b33a274bdea151e2c7804d6c9eedf21fb84de
Admin <rbroker@digital.ai>
2020-11-02 15:58:29 -0500

    remove unused artifacts

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ export VERSION="3.1.0"
 
 cd artifacts/aws.ec2_instance
 zip -r ../aws.ec2_instance.zip ./*
-# cd ../aws.rds
-# zip -r ../aws.rds.zip ./*
 
 cd ../..
 curl -LO https://dist.xebialabs.com/public/xl-cli/$CLI_VERSION/linux-amd64/xl
@@ -14,5 +12,4 @@ chmod +x xl
 ./xl apply --xl-deploy-url=$XLD_URL --xl-deploy-username=$XLD_USER --xl-deploy-password=$XLD_PASSWD --file petportalhosts.yaml --values version=$VERSION
 
 rm artifacts/aws.ec2_instance.zip
-# rm artifacts/aws.rds.zip
 rm xl
